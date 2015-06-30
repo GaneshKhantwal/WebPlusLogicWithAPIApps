852efdd9acab51c4dc52c1fed37c24c6ced1d54c
GaneshKhantwal <ganesh.khantwal@niit-tech.com>
2015-06-30 17:14:21 +0530

    add projects

---
+++ WebPlusLogicWithAPIApps/Models/OrderViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPlusLogicWithAPIApps.Models
+{
+    public class OrderViewModels
+    {
+            [Required]
+            [Display(Name = "Recipient Name")]
+            public string RecipientName { get; set; }
+
+            [Display(Name = "Street")]
+            public string DeliveryStreet { get; set; }
+
+            [Display(Name = "City")]
+            public string DeliveryCity { get; set; }
+
+            [Display(Name = "State")]
+            public string DeliveryState { get; set; }
+
+            [Display(Name = "Postal Code")]
+            public string DeliveryPostalCode { get; set; }
+
+            [Required]
+            [Display(Name = "Phone")]
+            public string Phone { get; set; }
+
+    }
+}
---
+++ WebPlusLogicWithAPIApps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPlusLogicWithAPIApps.Startup))]
+namespace WebPlusLogicWithAPIApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
